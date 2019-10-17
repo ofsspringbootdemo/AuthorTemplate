4249e5f86aa0cf1149be3af0720bed2f3652edc1
Thavasi Narayanan Sundaramoorthy <thavasi.s@itp.objectfrontier.com>
2019-10-17 18:33:09 +0530

    property file changes and Model class changes for sql connection

--- src/main/java/com/jana/jpademo/model/ArticleType.java
+++ src/main/java/com/jana/jpademo/model/ArticleType.java
@@ -13,118 +13,117 @@ import java.util.List;
 @Table(name = "ARTICLE_TYPE")
 public class ArticleType {
 
-    @Id
-    @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "articleTypeSeqGenerator")
-    @SequenceGenerator(name="articleTypeSeqGenerator", sequenceName = "ARTICLE_TYPE_SEQ", allocationSize=1)
-    @Column(name = "ID")
-    private Long id;
+	@Id
+	@GeneratedValue
+	@Column(name = "ID")
+	private Long id;
 
-    @Column(name = "NAME")
-    private String name;
+	@Column(name = "NAME")
+	private String name;
 
-    @Column(name = "DESCRIPTION")
-    private String description;
+	@Column(name = "DESCRIPTION")
+	private String description;
 
-    @Column(name = "STATUS")
-    private String status;
+	@Column(name = "STATUS")
+	private String status;
 
-    @Column(name = "CREATED_BY")
-    private String createdBy;
+	@Column(name = "CREATED_BY")
+	private String createdBy;
 
-    @Column(name = "CREATED_DATE")
-    private Date createdDate;
+	@Column(name = "CREATED_DATE")
+	private Date createdDate;
 
-    @Column(name = "MODIFIED_BY")
-    private String modifiedBy;
+	@Column(name = "MODIFIED_BY")
+	private String modifiedBy;
 
-    @Column(name = "MODIFIED_DATE")
-    private Date modifiedDate;
+	@Column(name = "MODIFIED_DATE")
+	private Date modifiedDate;
 
-    @ManyToOne
-    @JsonBackReference
-    private TemplateName templateName;
+	@ManyToOne
+	@JsonBackReference
+	private TemplateName templateName;
 
-    @OneToMany(cascade = CascadeType.ALL)
-    @JoinColumn(name = "ARTICLE_TYPE_ID")
-    private List<FileDownload> fileDownloadList;
+	@OneToMany(cascade = CascadeType.ALL)
+	@JoinColumn(name = "ARTICLE_TYPE_ID")
+	private List<FileDownload> fileDownloadList;
 
-    public Long getId() {
-        return id;
-    }
+	public Long getId() {
+		return id;
+	}
 
-    public void setId(Long id) {
-        this.id = id;
-    }
+	public void setId(Long id) {
+		this.id = id;
+	}
 
-    public String getName() {
-        return name;
-    }
+	public String getName() {
+		return name;
+	}
 
-    public void setName(String name) {
-        this.name = name;
-    }
+	public void setName(String name) {
+		this.name = name;
+	}
 
-    public String getDescription() {
-        return description;
-    }
+	public String getDescription() {
+		return description;
+	}
 
-    public void setDescription(String description) {
-        this.description = description;
-    }
+	public void setDescription(String description) {
+		this.description = description;
+	}
 
-    public String getStatus() {
-        return status;
-    }
+	public String getStatus() {
+		return status;
+	}
 
-    public void setStatus(String status) {
-        this.status = status;
-    }
+	public void setStatus(String status) {
+		this.status = status;
+	}
 
-    public String getCreatedBy() {
-        return createdBy;
-    }
+	public String getCreatedBy() {
+		return createdBy;
+	}
 
-    public void setCreatedBy(String createdBy) {
-        this.createdBy = createdBy;
-    }
+	public void setCreatedBy(String createdBy) {
+		this.createdBy = createdBy;
+	}
 
-    public Date getCreatedDate() {
-        return createdDate;
-    }
+	public Date getCreatedDate() {
+		return createdDate;
+	}
 
-    public void setCreatedDate(Date createdDate) {
-        this.createdDate = createdDate;
-    }
+	public void setCreatedDate(Date createdDate) {
+		this.createdDate = createdDate;
+	}
 
-    public String getModifiedBy() {
-        return modifiedBy;
-    }
+	public String getModifiedBy() {
+		return modifiedBy;
+	}
 
-    public void setModifiedBy(String modifiedBy) {
-        this.modifiedBy = modifiedBy;
-    }
+	public void setModifiedBy(String modifiedBy) {
+		this.modifiedBy = modifiedBy;
+	}
 
-    public Date getModifiedDate() {
-        return modifiedDate;
-    }
+	public Date getModifiedDate() {
+		return modifiedDate;
+	}
 
-    public void setModifiedDate(Date modifiedDate) {
-        this.modifiedDate = modifiedDate;
-    }
+	public void setModifiedDate(Date modifiedDate) {
+		this.modifiedDate = modifiedDate;
+	}
 
-    public TemplateName getTemplateName() {
-        return templateName;
-    }
+	public TemplateName getTemplateName() {
+		return templateName;
+	}
 
-    public void setTemplateName(TemplateName templateName) {
-        this.templateName = templateName;
-    }
+	public void setTemplateName(TemplateName templateName) {
+		this.templateName = templateName;
+	}
 
-    public List<FileDownload> getFileDownloadList() {
-        return fileDownloadList;
-    }
+	public List<FileDownload> getFileDownloadList() {
+		return fileDownloadList;
+	}
 
-    public void setFileDownloadList(List<FileDownload> fileDownloadList) {
-        this.fileDownloadList = fileDownloadList;
-    }
+	public void setFileDownloadList(List<FileDownload> fileDownloadList) {
+		this.fileDownloadList = fileDownloadList;
+	}
 }
--- src/main/java/com/jana/jpademo/model/FileDownload.java
+++ src/main/java/com/jana/jpademo/model/FileDownload.java
@@ -9,50 +9,49 @@ import javax.persistence.*;
 @Table(name = "FILE_DOWNLOAD")
 public class FileDownload {
 
-    @Id
-    @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "fileDownloadSeqGenerator")
-    @SequenceGenerator(name="fileDownloadSeqGenerator", sequenceName = "FILE_DOWNLOAD_SEQ", allocationSize=1)
-    @Column(name = "ID")
-    private Long id;
+	@Id
+	@GeneratedValue
+	@Column(name = "ID")
+	private Long id;
 
-    @Column(name = "FORMAT")
-    private String format;
+	@Column(name = "FORMAT")
+	private String format;
 
-    @Column(name = "URL")
-    private String url;
+	@Column(name = "URL")
+	private String url;
 
-    @Column(name = "DESCRIPTIVE_TEXT")
-    private String descriptiveText;
+	@Column(name = "DESCRIPTIVE_TEXT")
+	private String descriptiveText;
 
-    public Long getId() {
-        return id;
-    }
+	public Long getId() {
+		return id;
+	}
 
-    public void setId(Long id) {
-        this.id = id;
-    }
+	public void setId(Long id) {
+		this.id = id;
+	}
 
-    public String getFormat() {
-        return format;
-    }
+	public String getFormat() {
+		return format;
+	}
 
-    public void setFormat(String format) {
-        this.format = format;
-    }
+	public void setFormat(String format) {
+		this.format = format;
+	}
 
-    public String getUrl() {
-        return url;
-    }
+	public String getUrl() {
+		return url;
+	}
 
-    public void setUrl(String url) {
-        this.url = url;
-    }
+	public void setUrl(String url) {
+		this.url = url;
+	}
 
-    public String getDescriptiveText() {
-        return descriptiveText;
-    }
+	public String getDescriptiveText() {
+		return descriptiveText;
+	}
 
-    public void setDescriptiveText(String descriptiveText) {
-        this.descriptiveText = descriptiveText;
-    }
+	public void setDescriptiveText(String descriptiveText) {
+		this.descriptiveText = descriptiveText;
+	}
 }
