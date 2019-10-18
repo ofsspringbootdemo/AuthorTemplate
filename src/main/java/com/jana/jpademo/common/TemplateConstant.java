eb5547d9203fced5816d2011540657cb87b2793c
Thavasi Narayanan Sundaramoorthy <thavasi.s@itp.objectfrontier.com>
2019-10-18 19:50:49 +0530

    Logic for Download

--- src/main/java/com/jana/jpademo/common/TemplateConstant.java
+++ src/main/java/com/jana/jpademo/common/TemplateConstant.java
@@ -2,4 +2,5 @@ package com.jana.jpademo.common;
 
 public class TemplateConstant {
 	public static final String ErrMsg1 = "Publication Type is Empty";
+	public static final String file_location="D:/download/";
 }
