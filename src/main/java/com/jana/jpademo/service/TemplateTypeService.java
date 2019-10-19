8b1c85250c832366b4b621a3867b57a79846ad94
Thavasi Narayanan Sundaramoorthy <thavasi.s@itp.objectfrontier.com>
2019-10-19 12:22:14 +0530

    Moved the Logic to Controller

--- src/main/java/com/jana/jpademo/service/TemplateTypeService.java
+++ src/main/java/com/jana/jpademo/service/TemplateTypeService.java
@@ -21,17 +21,8 @@ import com.jana.jpademo.vo.ErrMessageVO;
 public class TemplateTypeService {
 
 	@Autowired
-	private TemplateNameService templateNameService;
-
-	@Autowired
-	private ArticleTypeService articleTypeService;
-
-	@Autowired
 	private TemplateTypeRepository templateTypeRepository;
-
-	@Autowired
-	private FileDownloadService fileDownloadService;
-
+	
 	public void saveOrUpdate(TemplateType templateType) {
 		templateTypeRepository.save(templateType);
 	}
@@ -46,39 +37,6 @@ public class TemplateTypeService {
 
 	public void deleteTemplateType(Long id) {
 		templateTypeRepository.deleteById(id);
-	}
-
-	public AuthTemplateVO getAuthTemplate(String templateTypeID, String templateNameID, String articleTypeID) {
-		AuthTemplateVO authTemplateVO = new AuthTemplateVO();
-		if (!StringUtils.isEmpty(articleTypeID)) {
-			authTemplateVO
-					.setDownload(fileDownloadService.getFileDownloadsByArticleTypeId(Long.valueOf(articleTypeID)));
-		} else if (!StringUtils.isEmpty(templateNameID)) {
-
-			List<FileDownload> fileDownloadaByTemplateName = fileDownloadService
-					.getFileDownloadsByTemplateNameId(Long.valueOf(templateNameID));
-			if (!fileDownloadaByTemplateName.isEmpty()) {
-				authTemplateVO.setDownload(fileDownloadaByTemplateName);
-			} else {
-				authTemplateVO.setArticleType(
-						articleTypeService.getArticleTypesByTemplateNameId(Long.valueOf(templateNameID)));
-			}
-		} else if (!StringUtils.isEmpty(templateTypeID)) {
-			List<FileDownload> fileDownloadaByTemplateType = fileDownloadService
-					.getFileDownloadsByTemplateTypeId(Long.valueOf(templateTypeID));
-			if (!fileDownloadaByTemplateType.isEmpty()) {
-				authTemplateVO.setDownload(fileDownloadaByTemplateType);
-			} else {
-				authTemplateVO.setTemplateName(
-						templateNameService.getTemplateNamesByTemplateTypeId(Long.valueOf(templateTypeID)));
-			}
-		} else {
-			System.out.println(TemplateConstant.ErrMsg1);
-			ErrMessageVO errMsg = new ErrMessageVO();
-			errMsg.setErrMsg(TemplateConstant.ErrMsg1);
-			authTemplateVO.setErrMsgDtls(errMsg);
-		}
-		return authTemplateVO;
 	}
 
 }
